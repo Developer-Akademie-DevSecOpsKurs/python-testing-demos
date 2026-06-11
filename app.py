1495d7d05e9351a57d9e74e2774e1d1a13fcc698
Sven Patrick Meier <sven.patrick.meier@gmail.com>
2026-06-11 20:34:07 +0200

    feat: allow for multiple output formats
    implement weeks, days, hours, seconds as possible output formats

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-
 import argparse
 import datetime
 import re
+
+
+def calculate_weeks(target_date: str) -> float:
+    # TODO: implement this
+    # calc target_date - current_date
+    assert isinstance(target_date, str), "Invalid input type"
+    assert re.match(r"^\d{4}-\d{2}-\d{2}$", target_date), "Invalid date format"
+
+    current_date: datetime.date = datetime.date.today()
+    target_date: datetime.date = datetime.date.fromisoformat(target_date)
+
+    remaining_time_in_days = (target_date - current_date)
+
+    remaining_time_weeks = remaining_time_in_days.total_seconds() / 60 / 60 / 24 / 7
+
+    return normalize_time_output(remaining_time_weeks)
 
 
 def calculate_days(target_date: str) -> int:
@@ -9,8 +24,8 @@ def calculate_days(target_date: str) -> int:
     assert isinstance(target_date, str), "Invalid input type"
     assert re.match(r"^\d{4}-\d{2}-\d{2}$", target_date), "Invalid date format"
 
-    current_date: datetime = datetime.date.today()
-    target_date: datetime = datetime.date.fromisoformat(target_date)
+    current_date: datetime.date = datetime.date.today()
+    target_date: datetime.date = datetime.date.fromisoformat(target_date)
 
     if target_date < current_date:
         raise ValueError("Date cannot be in the past")
@@ -18,6 +33,38 @@ def calculate_days(target_date: str) -> int:
     remaining_time = target_date - current_date
 
     return remaining_time.days
+
+
+def calculate_hours(target_date: str) -> float:
+    # calc target_date - current_date
+    assert isinstance(target_date, str), "Invalid input type"
+    assert re.match(r"^\d{4}-\d{2}-\d{2}$", target_date), "Invalid date format"
+
+    current_date: datetime.date = datetime.date.today()
+    target_date: datetime.date = datetime.date.fromisoformat(target_date)
+
+    remaining_time_in_days = (target_date - current_date)
+
+    remaining_time_hours = remaining_time_in_days.total_seconds() / 60 / 60
+
+    return normalize_time_output(remaining_time_hours)
+
+
+def calculate_seconds(target_date: str) -> float:
+    # calc target_date - current_date
+    assert isinstance(target_date, str), "Invalid input type"
+    assert re.match(r"^\d{4}-\d{2}-\d{2}$", target_date), "Invalid date format"
+
+    current_date: datetime.date = datetime.date.today()
+    target_date: datetime.date = datetime.date.fromisoformat(target_date)
+
+    remaining_time_in_days = (target_date - current_date)
+
+    return normalize_time_output(remaining_time_in_days.total_seconds())
+
+
+def normalize_time_output(raw_time: float) -> str:
+    return "{:.2f}".format(round(raw_time, 2))
 
 
 def cli_entrypoint():
@@ -28,15 +75,35 @@ def cli_entrypoint():
         "target_date",
         help="Target date in ISO format, e.g. 2026-04-20",
     )
+    parser.add_argument(
+        "-o",
+        "--output-format",
+        choices=["seconds", "hours", "weeks"],
+        help="Can be used to specify time output format. Defaults to full days"
+    )
 
     args = parser.parse_args()
 
     try:
-        days = calculate_days(args.target_date)
-        print(f"From today to {args.target_date} it is {days} full days remaining")
-    except (AssertionError):
+        unit = "days"
+        if args.output_format:
+            if args.output_format == "seconds":
+                result = calculate_seconds(args.target_date)
+                unit = "seconds"
+            if args.output_format == "hours":
+                result = calculate_hours(args.target_date)
+                unit = "hours"
+
+            if args.output_format == "weeks":
+                result = calculate_weeks(args.target_date)
+                unit = "weeks"
+        else:
+            result = calculate_days(args.target_date)
+
+        print(f"From today to {args.target_date} it is {result} {unit} remaining")  # noqa: E501
+    except AssertionError:
         print("Invalid date format. Use YYYY-MM-DD (example: 2026-04-20).")
-    except (ValueError):
+    except ValueError:
         print("Date cannot be in the past")
     except Exception as e:
         print("An unexpected error occurred.")
